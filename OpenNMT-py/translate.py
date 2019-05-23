bf34ca70bc1c0c5d778610064e393864ba79dd08
Tester <code2vec.critique@gmail.com>
2019-05-23 16:10:06 +0800

    code for comment gen

---
+++ OpenNMT-py/translate.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+import configargparse
+from onmt.utils.logging import init_logger
+from onmt.utils.misc import split_corpus
+from onmt.translate.translator import build_translator
+
+import onmt.opts as opts
+
+
+def main(opt):
+    translator = build_translator(opt, report_score=True)
+    src_shards = split_corpus(opt.src, opt.shard_size)
+    tgt_shards = split_corpus(opt.tgt, opt.shard_size) \
+        if opt.tgt is not None else [None]*opt.shard_size
+    shard_pairs = zip(src_shards, tgt_shards)
+
+    for i, (src_shard, tgt_shard) in enumerate(shard_pairs):
+        logger.info("Translating shard %d." % i)
+        translator.translate(
+            src=src_shard,
+            tgt=tgt_shard,
+            src_dir=opt.src_dir,
+            batch_size=opt.batch_size,
+            attn_debug=opt.attn_debug
+            )
+
+
+if __name__ == "__main__":
+    parser = configargparse.ArgumentParser(
+        description='translate.py',
+        config_file_parser_class=configargparse.YAMLConfigFileParser,
+        formatter_class=configargparse.ArgumentDefaultsHelpFormatter)
+    opts.config_opts(parser)
+    opts.add_md_help_argument(parser)
+    opts.translate_opts(parser)
+
+    opt = parser.parse_args()
+    logger = init_logger(opt.log_file)
+    main(opt)
