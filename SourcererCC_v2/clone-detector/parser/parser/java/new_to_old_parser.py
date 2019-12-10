f6746ef7d18fa535139ffcc70285c40a41ceee69
Tester <code2vec.critique@gmail.com>
2019-12-10 10:40:27 +0800

    fix extra subdirectory

--- SourcererCC_v2/clone-detector/parser/parser/java/new_to_old_parser.py
+++
@@ -1,8 +0,0 @@
-
-
-with open('/Users/kanghongjin/repos/SourcererCC_70_mil_1e/tokenizers/file-level/blocks.file', 'r') as infile:
-    for line in infile:
-        preamble = line.split('@#@')[0]
-        splitted = preamble.split(',')
-        new_output = ','.join(splitted[:2]) + '@#@' + line.split('@#@')[1].rstrip()
-        print(new_output)
